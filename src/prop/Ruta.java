5019b723da3633aff04b77763a50971e6ce9d11d
josedavila04 <59580613+josedavila04@users.noreply.github.com>
2020-02-07 11:13:21 -0500

    Update Ruta.java

--- src/prop/Ruta.java
+++ src/prop/Ruta.java
@@ -14,6 +14,8 @@ import com.teamdev.jxmaps.swing.MapView;
 
 import com.teamdev.jxmaps.*;
 import com.teamdev.jxmaps.examples.ControlPanel;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Ruta extends MapView {
 
@@ -23,13 +25,6 @@ public class Ruta extends MapView {
 
     public PolylineOptions settingsLine;
 
-    public CircleOptions getSettingsCircle() {
-        return settingsCircle;
-    }
-
-    public void setSettingsCircle(CircleOptions settingsCircle) {
-        this.settingsCircle = settingsCircle;
-    }
     public Marker generateMarker(LatLng pos) {
         Marker marker = null;
 
@@ -37,10 +32,9 @@ public class Ruta extends MapView {
         marker.setPosition(pos);
         //map.setCenter(pos);
 
-        System.out.println("Graficado marker");
+        System.out.println("Graficado Marcador");
         return marker;
-        
-        
+
     }
 
     public void generarRuta(LatLng start, LatLng end, Boolean markers) {
@@ -60,42 +54,69 @@ public class Ruta extends MapView {
         map.setOptions(mapOptions);
         map.setCenter(new LatLng(-1.6709800, -78.6471200));
         map.setZoom(7);
-       PolylineOptions options = new PolylineOptions();
-       options.setStrokeColor("#1E90FF");
-       polyline.setOptions(options);
+        PolylineOptions options = new PolylineOptions();
+        options.setStrokeColor("#1E90FF");
+        polyline.setOptions(options);
     }
 
-    public Double calcularDistancia(LatLng a,LatLng b){
-        Double distancia1 = Math.sqrt(Math.pow(a.getLat(), 2) + Math.pow(a.getLng(), 2));
-   Double distancia2 = Math.sqrt(Math.pow(b.getLat(), 2) + Math.pow(b.getLng(), 2)); 
-   Double distancia=Math.abs(distancia1-distancia2);
+    public float calcularDistancia(LatLng a, LatLng b) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        float distancia1 = (float) Math.sqrt(Math.pow(a.getLat(), 2) + Math.pow(a.getLng(), 2));
+        float distancia2 = (float) Math.sqrt(Math.pow(b.getLat(), 2) + Math.pow(b.getLng(), 2));
+        float distancia = (float) ((Math.abs(distancia1 - distancia2)) * 1000 * 0.30);
         return distancia;
+    }
+
+    public float calcularTiempo(float distancia) {
+
+        float tiempo = 0;
+        float velocidad = 60;
+       // tiempo =Math.round(distancia / velocidad);
+       tiempo =(distancia / velocidad );
+        
+        return tiempo;
+
+    }
+
+    public float calcularGananciaAPP(float distancia) {
+
+        
+        float valor = (float) (distancia * 0.06);
+        float ganancia = (float) ((valor * 0.2*100)/100);
+//        System.out.println(ganancia);
+   
+ 
+        return ganancia;
 
     }
     
+    public float calcularMonto(float distancia, float ganancia) {
+
+        
+        float valor = (float) (distancia * 0.0625);
+        float monto = (valor + ganancia);
+
+        return monto;
+
+    }
+    public void generarRutaDijkstra()
+    {
+      final map.Ruta example = new map.Ruta("PoliDriver");
+    float distancia = 0;
+    float tiempo = 0;
+    float monto = 0;
+    float gananciaPolidriver = 0;
+    
+     
+    }
+
+    
+    
+
     public Map darMap() {
         return map;
     }
 
-    public void generateArea(LatLng center, Double radius) {
-        Circle circle = new Circle(map);
-        circle.setCenter(center);
-        circle.setRadius(radius);
-        circle.setVisible(true);
-        circle.setOptions(settingsCircle);
-    }
-
-    public void GenerateLine(boolean markers, LatLng... path) {
-        if (markers) {
-            for (LatLng p : path) {
-                generateMarker(p);
-            }
-        }
-        Polyline polyline = new Polyline(map);
-        polyline.setPath(path);
-    }
-
- 
     public Ruta(String pString) {
 
         pString = "";
@@ -116,7 +137,6 @@ public class Ruta extends MapView {
         settingsLine.setStrokeOpacity(1.0);
         settingsLine.setStrokeWeight(2.0);
 
-       
         setOnMapReadyHandler(new MapReadyHandler() {
             @Override
             public void onMapReady(MapStatus status) {
@@ -137,11 +157,10 @@ public class Ruta extends MapView {
             }
         });
 
-
     }
 
     public static void main(String[] args) {
-   
+
     }
 
 }
